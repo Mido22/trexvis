5439a0248ea1a8d89ccc4f6b94117a144ae3fdc8
mido22 <ban.mido22@gmail.com>
2018-03-03 17:22:37 +0100

    minor code change

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { Button, Form, Card, Image } from 'semantic-ui-react'
+import { Button, Form, Card, Image, Modal, Header, Icon } from 'semantic-ui-react'
 import sportsImg from '../static/home_sport.jpg';
 import suvImg from '../static/home_suv.jpg';
 import luxuryImg from '../static/home_luxury.jpg';
 import './home.css';
 
 class Home extends Component {
+  state = {}
+
+  handleChange = (e, { name, value }) => {
+    console.log(e, name, value)
+    this.setState({ [name]: value })
+  }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+
+  handleSubmit = () => {
+    let outputText = '\n'
+    console.log('Send this somewhere: ', outputText)
+    Object.keys(this.state).forEach( key => {
+      if (![
+        'name', 'email', 'vehicle', 'pickup_location',
+      'telephone', 'pickup_date', 'pickup_time', 'dropoff_date', 'dropoff_time'
+      ].includes(key)) 
+        return
+      
+      outputText += `\n ${key}:  ${this.state[key]}`
+    })
+
+    console.log('Send this somewhere: ', outputText)
+    this.sendMail()
+  }
+
+  sendMail = (text) => {
+    fetch('https://0tp3ssvsdf.execute-api.eu-west-1.amazonaws.com/trexvisMailAPI/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: text
+    }).then(() => this.handleOpen()).catch(console.log)
+  }
+
   render() {
     let vehicleOptions = [
       { text: 'Toyota Etios Liva', value: 'Toyota Etios Liva' },
@@ -24,19 +65,41 @@ class Home extends Component {
       { text: 'Tempo Traveller', value: 'Tempo Traveller' },
     ];
 
+    const { 
+      name, email, vehicle, pickup_location,
+      telephone, pickup_date, pickup_time, dropoff_date, dropoff_time
+     } = this.state;
+
     return (
       <div className="home">
         <div className="form">
-          <Form>
+          <Modal
+            open={this.state.modalOpen}
+            onClose={this.handleClose}
+            basic
+            size='small'
+          >
+            <Header icon='browser' content='Form Submitted' />
+            <Modal.Content>
+              <h3>We have received your request, would get back to you soon :)</h3>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='green' onClick={this.handleClose} inverted>
+                <Icon name='checkmark' /> Okay
+              </Button>
+            </Modal.Actions>
+          </Modal>
+          <Form  onSubmit={this.handleSubmit}>
             <h3>Get a Quote</h3>
-            <Form.Input fluid label='Name' placeholder='Name' inverted={true} />
-            <Form.Select fluid label='Vehicle Type' placeholder='Vehicle Type' options={vehicleOptions} />
-            <Form.Input fluid label='Pickup Location' placeholder='Pickup Location' />
-            <Form.Input fluid label='Telephone' placeholder='Telephone' type="tel"/>
-            <Form.Input fluid label='Pickup date' type="date" placeholder='Pickup date' />
-            <Form.Input fluid label='Pickup time' type="time" placeholder='Pickup time' />
-            <Form.Input fluid label='Drop-off date' type="date" placeholder='Drop-off date' />
-            <Form.Input fluid label='Drop-off time' type="time" placeholder='Drop-off time' />
+            <Form.Input fluid label='Name' value={name} placeholder='Name' name="name" inverted={true}  onChange={this.handleChange} />
+            <Form.Select fluid label='Vehicle Type' value={vehicle} name="vehicle" placeholder='Vehicle Type' options={vehicleOptions}  onChange={this.handleChange} />
+            <Form.Input fluid label='Pickup Location' value={pickup_location} name="pickup_location" placeholder='Pickup Location'  onChange={this.handleChange} />
+            <Form.Input fluid label='Telephone' value={telephone} name="telephone" placeholder='Telephone' type="tel"  onChange={this.handleChange} />
+            <Form.Input fluid label='Email' value={email} name="email" placeholder='Email' type="email"  onChange={this.handleChange} />
+            <Form.Input fluid label='Pickup date' value={pickup_date} name="pickup_date" type="date" placeholder='Pickup date'  onChange={this.handleChange} />
+            <Form.Input fluid label='Pickup time' value={pickup_time} name="pickup_time" type="time" placeholder='Pickup time'  onChange={this.handleChange} />
+            <Form.Input fluid label='Drop-off date' value={dropoff_date} name="dropoff_date" type="date" placeholder='Drop-off date'  onChange={this.handleChange} />
+            <Form.Input fluid label='Drop-off time' value={dropoff_time} name="dropoff_time" type="time" placeholder='Drop-off time'  onChange={this.handleChange} />
             <Button type='submit'>Submit</Button>
           </Form>
         </div>
